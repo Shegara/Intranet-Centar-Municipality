484b75a3655300f3c8a7215e544dd2de678e848b
Shegara <markomateosego@gmail.com>
2024-09-20 10:23:54 +0200

    feat: added navigation links to cms homepage

--- intranet-admin/app/pages/docs/page.js
+++ intranet-admin/app/pages/docs/page.js
@@ -1,23 +1,24 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import "react-toastify/dist/ReactToastify.css";
-import CreateDocs from "../../components/docs/createDocs";  
-import UpdateDocs from "../../components/docs/updateDocs";  
-import DeleteDocs from "../../components/docs/deleteDocs";  
+import CreateDocs from "../../components/docs/createDocs";
+import UpdateDocs from "../../components/docs/updateDocs";
+import DeleteDocs from "../../components/docs/deleteDocs";
 
-const Docs = () => { 
-  
-    return (
-      <div>
-        <CreateDocs />
-        <UpdateDocs />
-        <DeleteDocs />  
-      </div>
+const Docs = () => {
+  return (
+    <div className="relative">
+      <Link href="/" className="absolute top-10 left-10 text-blue-500 flex items-center">
+        <span>&#8592;</span>
+        <span className="ml-2">Početna stranica</span>
+      </Link>
+      <CreateDocs />
+      <UpdateDocs />
+      <DeleteDocs />
+    </div>
   );
-
-}
-
-
+};
 
 export default Docs;
--- intranet-admin/app/pages/users/page.js
+++ intranet-admin/app/pages/users/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import "react-toastify/dist/ReactToastify.css";
 import CreateUser from "../../components/users/createUser";  
 import UpdateUser from "../../components/users/updateUser";  
@@ -9,7 +10,11 @@ import DeleteUser from "../../components/users/deleteUser";
 const Users = () => { 
   
     return (
-      <div>
+      <div className="relative">
+        <Link href="/" className="absolute top-10 left-10 text-blue-500 flex items-center">
+          <span>&#8592;</span>
+          <span className="ml-2">Početna stranica</span>
+        </Link>
         <CreateUser  />
         <UpdateUser />
         <DeleteUser />  
